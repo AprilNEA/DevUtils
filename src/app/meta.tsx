6adfb2683f0c5c2c563e041e5265003d408a4d6f
AprilNEA <github@sku.moe>
2023-12-16 17:34:08 +0800

    feat: add unix time converter

--- src/app/meta.tsx
+++ src/app/meta.tsx
@@ -1,11 +1,26 @@
-import { JsonIcon, JwtIcon } from '@/icons';
+import { atom } from 'jotai';
+import { Metadata } from 'next';
+
+import { ClockIcon, JsonIcon, JwtIcon } from '@/icons';
+
+export enum Utils {
+  UnixTimeConverter = 'unix-time-converter',
+  JSONFormatValidate = 'json-format-validate',
+  JwtDebugger = 'jwt-debugger',
+}
+
+type metaProps = {
+  title: string;
+  icon: JSX.ElementType;
+};
 
 const meta: {
-  [key: string]: {
-    title: string;
-    icon: JSX.ElementType;
-  };
+  [key in Utils]: metaProps;
 } = {
+  'unix-time-converter': {
+    title: 'Unix Time Converter',
+    icon: ClockIcon,
+  },
   'json-format-validate': {
     title: 'JSON Format/Validate',
     icon: JsonIcon,
@@ -14,6 +29,12 @@ const meta: {
     title: 'JWT Debugger',
     icon: JwtIcon,
   },
-} as const;
+};
+
+export const getWebMeta = (key: Utils): Metadata => {
+  return {
+    title: `${meta[key].title} | DevUtils`,
+  };
+};
 
 export default meta;
