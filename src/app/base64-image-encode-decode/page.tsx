61248485acb6c56a7596acc1a4dc27d4f071b6f7
AprilNEA <github@sku.moe>
2023-12-18 01:59:58 +0800

    fix: fix meta typo

--- src/app/base64-image-encode-decode/page.tsx
+++ src/app/base64-image-encode-decode/page.tsx
@@ -2,7 +2,7 @@ import { Utils, getWebMeta } from '@/app/meta';
 
 import { Base64ImageEncodeDecode } from './base64-image-encode-decode';
 
-export const metadata = getWebMeta(Utils.JSONFormatValidate);
+export const metadata = getWebMeta(Utils.Base64ImageEncodeDecode);
 
 export default function Page() {
   return <Base64ImageEncodeDecode />;
--- src/app/base64-string-encode-decode/page.tsx
+++ src/app/base64-string-encode-decode/page.tsx
@@ -1,7 +1,7 @@
 import { Base64StringEncodeDecode } from '@/app/base64-string-encode-decode/base64-string-encode-decode';
 import { Utils, getWebMeta } from '@/app/meta';
 
-export const metadata = getWebMeta(Utils.JSONFormatValidate);
+export const metadata = getWebMeta(Utils.Base64StringEncodeDecode);
 
 export default function Page() {
   return <Base64StringEncodeDecode />;
