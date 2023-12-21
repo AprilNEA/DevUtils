b0313eb5b19048b166ba7691433771ce8c171ae9
AprilNEA <github@sku.moe>
2023-12-22 00:08:07 +0800

    feat: add new icon for category

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,12 @@
+//category
+export { default as ConvertIcon } from './convert.svg';
+export { default as EncodeIcon } from './encode.svg';
+export { default as FormatterIcon } from './format.svg';
+export { default as GeneratorIcon } from './generator.svg';
+export { default as GraphicIcon } from './graphic.svg';
+export { default as TextIcon } from './text.svg';
+
+// utils
 export { default as Base64Icon } from './base64.svg';
 export { default as Base64ImageIcon } from './base64image.svg';
 export { default as ChipIcon } from './chip.svg';
